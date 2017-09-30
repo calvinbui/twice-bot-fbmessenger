32d55595684dd83d02cc95f4e052841923642535
Calvin Bui <c@lvin.email>
2017-09-30 19:31:21 +1000

    Query database for matches and reply with result

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ var login = require("facebook-chat-api");
 var fs = require('fs');
 var db = JSON.parse(fs.readFileSync('database.json', 'utf8'));
 
-// strings to match
-const tt = /\btt\b/i
-const tabletennis = /\btable tennis\b/i
-const knockknock = /\bknock knock\b/i
-const ohah = /\boh+ ah+\b/i
-const signal = /\bsignal\b/i
-const cheerup = /\bcheer up\b/i
-const touchdown = /\btouchdown\b/i
-const td = /\bTD\b/i
-
 login({email: "", password: ""}, (err, api) => {
   if(err) return console.error(err);
 
@@ -20,22 +10,23 @@ login({email: "", password: ""}, (err, api) => {
   });
 
   api.listen((err, message) => {
-    console.log(message);
 
     function video(videoname, link) {
-      console.log("Replying with " + videoname);
+      console.log("Replying with " + videoname + " and " + link);
       api.sendMessage({
         url: link
         }, message.threadID
       );
     }
 
-    if (tt.test(message.body) || tabletennis.test(message.body)) { video("TT", "https://youtu.be/ePpPVE-GGJw?t=27s") }
-    else if (knockknock.test(message.body)) { video("Knock Knock", "https://youtu.be/8A2t_tAjMz8") }
-    else if (cheerup.test(message.body)) { video("Knock Knock", "https://youtu.be/c7rCyll5AeY") }
-    else if (ohah.test(message.body)) { video("Like OOH-AHH", "https://youtu.be/0rtV5esQT6I") }
-    else if (signal.test(message.body)) { video("Signal", "https://youtu.be/VQtonf1fv_s?t=35s") }
-    else if (touchdown.test(message.body) || td.test(message.body)) { video("Touchdown", "https://youtu.be/N1oYWMGeUTY") }
+    for (song in db.songs) {
+      for (query in db.songs[song].queries) {
+        var regexp = new RegExp("\\b" + db.songs[song].queries[query] + "\\b", 'i');
+        if (regexp.test(message.body)) {
+          video(db.songs[song].title, db.songs[song].media[Math.floor(Math.random()*db.songs[song].media.length)])
+        }
+      }
+    }
 
   });
 });
