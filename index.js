17cacb0b6998fa910efb8d0c889d9bc74da0083e
Calvin Bui <c@lvin.email>
2017-09-30 19:33:19 +1000

    Comments

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var db = JSON.parse(fs.readFileSync('database.json', 'utf8'));
 login({email: "", password: ""}, (err, api) => {
   if(err) return console.error(err);
 
+  // reply when i talk to myself
   api.setOptions({
     selfListen: true
   });
 
   api.listen((err, message) => {
 
+    // send message with media
     function video(videoname, link) {
       console.log("Replying with " + videoname + " and " + link);
       api.sendMessage({
@@ -19,10 +21,15 @@ login({email: "", password: ""}, (err, api) => {
       );
     }
 
+    // loop through songs array
     for (song in db.songs) {
+      // loop through each song's search queries
       for (query in db.songs[song].queries) {
+        // create regex expression
         var regexp = new RegExp("\\b" + db.songs[song].queries[query] + "\\b", 'i');
+        // test if regex matches
         if (regexp.test(message.body)) {
+          // call video file with random media file
           video(db.songs[song].title, db.songs[song].media[Math.floor(Math.random()*db.songs[song].media.length)])
         }
       }
